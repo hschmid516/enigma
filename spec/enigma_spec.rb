e70f70baa28a72a802f81c1351c96a3e7c30bbc4
Henry Schmid <henry.schmid1@gmail.com>
2021-08-05 16:44:52 -0600

    Add tests for using today's date and random key if none given

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -19,4 +19,18 @@ RSpec.describe Enigma do
     }
     expect(@enigma.encrypt('hello world', '02715', '040895')).to eq(expected)
   end
+
+  it "uses today's date if none given" do
+    expected = {
+      encryption: 'hello world',
+      key: '02715',
+      date: '050821'
+    }
+    expect(@enigma.encrypt('hello world', '02715')).to eq(expected)
+  end
+
+  it "can generate a random key and use today's date if none given" do
+    expect(@enigma.encrypt('hello world')[:key]).to be_a(String)
+    expect(@enigma.encrypt('hello world')[:key].length).to eq(5)
+  end
 end
