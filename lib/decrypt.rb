9a48f710853a62add9b7772c4975c5ba35817694
Henry Schmid <henry.schmid1@gmail.com>
2021-08-06 15:01:16 -0600

    Refactor encrypt and decrypt files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,21 +1,21 @@
 require './lib/enigma'
 
-if ARGV[0..1] != ['encrypted.txt', 'decrypted.txt']
-  puts "Please enter file names with the following format:\n
-         encrypted.txt decrypted.txt"
+if ARGV[0..1] != ['encrypted.txt', 'decrypted.txt'] ||
+  ARGV[2].class != Integer && ARGV[2].to_s.length !=5 ||
+  ARGV[3].class != Integer && ARGV[3].to_s.length !=6
+  puts "Please enter file names, key, and date with the following format:\n
+    encrypted.txt decrypted.txt <5-digit key> <DDMMYY>"
 else
    message = File.read(ARGV[0]).chomp
+   key = ARGV[2].to_s
+   date = ARGV[3].to_s
+
+   enigma = Enigma.new
+   enigma.decrypt(message, key, date)
+
+   decrypted = File.open('decrypted.txt', 'w')
+   decrypted.write(enigma.decrypted[:decryption])
+   decrypted.close
+
+   puts "Created 'decrypted.txt' with the key: #{key}, and date: #{date}"
 end
-
-key = '54728'
-date = '231230'
-
-enigma = Enigma.new
-enigma.decrypt(message, key, date)
-
-decrypted = File.open('decrypted.txt', 'w')
-require 'pry'; binding.pry
-decrypted.write(enigma.decrypted[:decryption])
-decrypted.close
-
-puts "Created 'decrypted.txt' with the key #{key} and date #{date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,16 +5,15 @@ if ARGV != ['message.txt', 'encrypted.txt']
          message.txt encrypted.txt"
 else
    message = File.read(ARGV[0]).chomp
+   key = '54728'
+   date = '231230'
+
+   enigma = Enigma.new
+   enigma.encrypt(message, key, date)
+
+   encrypted = File.open('encrypted.txt', 'w')
+   encrypted.write(enigma.encrypted[:encryption])
+   encrypted.close
+
+   puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
 end
-
-key = '54728'
-date = '231230'
-
-enigma = Enigma.new
-enigma.encrypt(message, key, date)
-
-encrypted = File.open('encrypted.txt', 'w')
-encrypted.write(enigma.encrypted[:encryption])
-encrypted.close
-
-puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
