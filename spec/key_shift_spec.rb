f9e65dbb37a6f99aa1e92f36038df6411405395b
Henry Schmid <henry.schmid1@gmail.com>
2021-08-08 14:10:36 -0600

    Remove decryption tests, add attribute and keys_offsets tests

--- spec/key_shift_spec.rb
+++ spec/key_shift_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe KeyShift do
     it 'exists' do
       expect(key_shift).to be_a(KeyShift)
     end
+
+    it 'has attributes' do
+      expect(key_shift.key).to eq('02715')
+      expect(key_shift.date).to eq('040895')
+    end
   end
 
   context 'keys and shifts' do
@@ -24,39 +29,9 @@ RSpec.describe KeyShift do
       expect(key_shift.all_shifts).to eq([3, 27, 73, 20])
     end
 
-    it 'makes a character set' do
-      expected =
-      ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-      expect(key_shift.character_set).to eq(expected)
+    it 'makes array of keys with offsets' do
+      expected = [["02", "1"], ["27", "0"], ["71", "2"], ["15", "5"]]
+      expect(key_shift.keys_offsets).to eq(expected)
     end
   end
-
-  # context 'decryption' do
-  #   decrypter = Decrypter.new
-  #
-  #   it 'can decrypt characters' do
-  #     decrypter.decryption('keder ohulw', '02715', '040895')
-  #
-  #     expect(decrypter.crypt_chars(['k', 'e', 'd'], decrypter.decrypted[:key], decrypter.decrypted[:date], :-)).to eq('hel')
-  #   end
-  #
-  #   it 'decrypts a message' do
-  #     expected = {
-  #       decryption: 'hello world',
-  #       key: '02715',
-  #       date: '040895'
-  #     }
-  #     expect(decrypter.decryption('keder ohulw', '02715', '040895')).to eq(expected)
-  #   end
-  #
-  #   it 'has decrypted data in @decrypted' do
-  #     expected = {
-  #       decryption: 'hello world',
-  #       key: '02715',
-  #       date: '040895'
-  #     }
-  #     expect(decrypter.decrypted).to eq(expected)
-  #   end
-  # end
 end
