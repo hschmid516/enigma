fc0ba94f18605eb5588fe08330d472271dac21f2
Henry Schmid <henry.schmid1@gmail.com>
2021-08-08 14:12:59 -0600

    Refactor encryption and decryption tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -21,34 +21,63 @@ RSpec.describe Enigma do
       expect(enigma.encrypt('hello world', '02715', '040895')).to eq(expected)
     end
 
-    # Today's date works for this August, use mock so test always passes
     it "uses today's date if none given" do
+      allow(enigma).to receive(:make_date).and_return('080821')
+
       expected = {
         encryption: 'nefau qdxly',
         key: '02715',
-        date: Date.today.strftime('%d%m%y')
+        date: '080821'
       }
       expect(enigma.encrypt('hello world', '02715')).to eq(expected)
     end
 
     it "can generate a random key and use today's date if none given" do
-      expect(enigma.encrypt('hello world')[:key]).to be_a(String)
-      expect(enigma.encrypt('hello world')[:key].length).to eq(5)
-      expect(enigma.encrypt('hello world')[:date]).to eq(Date.today.strftime('%d%m%y'))
+      allow(enigma).to receive(:make_date).and_return('080821')
+      allow(enigma).to receive(:random_key).and_return('74812')
+
+      expected = {
+        encryption: 'ezpylu aofh',
+        key: '74812',
+        date: '080821'
+      }
+      expect(enigma.encrypt('hello world')).to eq(expected)
     end
   end
 
   context 'decryption' do
     enigma = Enigma.new
 
-    # Today's date works for this August, use mock so test always passes
-    it "uses today's date if none given" do
+    it 'can decrypt a message' do
       expected = {
         decryption: 'hello world',
         key: '02715',
-        date: Date.today.strftime('%d%m%y')
+        date: '040895'
+      }
+      expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq(expected)
+    end
+    
+    it "uses today's date if none given" do
+      allow(enigma).to receive(:make_date).and_return('080821')
+
+      expected = {
+        decryption: 'hello world',
+        key: '02715',
+        date: '080821'
       }
       expect(enigma.decrypt('nefau qdxly', '02715')).to eq(expected)
+    end
+
+    it "can generate a random key and use today's date if none given" do
+      allow(enigma).to receive(:make_date).and_return('080821')
+      allow(enigma).to receive(:random_key).and_return('74812')
+
+      expected = {
+        decryption: 'hello world',
+        key: '74812',
+        date: '080821'
+      }
+      expect(enigma.decrypt('ezpylu aofh')).to eq(expected)
     end
   end
 end
