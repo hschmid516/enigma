9ff589a04d8c934a8a40ea3b82898133a8691b4e
Henry Schmid <henry.schmid1@gmail.com>
2021-08-09 16:25:13 -0600

    Refactor method names

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -19,17 +19,17 @@ RSpec.describe Cipher do
     cipher = Cipher.new('hello world', '02715', '040895', :+)
 
     it 'can encrypt characters' do
-      expect(cipher.crypt_chars(['h', 'e', 'l', 'l'])).to eq('kede')
+      expect(cipher.cipher_chars(['h', 'e', 'l', 'l'])).to eq('kede')
     end
 
     it 'encrypts a message' do
-      expect(cipher.crypt_text).to eq('keder ohulw')
+      expect(cipher.cipher_text).to eq('keder ohulw')
     end
 
     it 'rotates chars' do
       expected = ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
         "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-      expect(cipher.rotate_chars(3, 'h')).to eq(expected)
+      expect(cipher.rotate_characters(3, 'h')).to eq(expected)
     end
 
     it 'creates arrays of characters with shifts' do
@@ -49,17 +49,17 @@ RSpec.describe Cipher do
     cipher = Cipher.new('keder ohulw', '02715', '040895', :-)
 
     it 'can decrypt characters' do
-      expect(cipher.crypt_chars(['k', 'e', 'd', 'e'])).to eq('hell')
+      expect(cipher.cipher_chars(['k', 'e', 'd', 'e'])).to eq('hell')
     end
 
     it 'decrypts a message' do
-      expect(cipher.crypt_text).to eq('hello world')
+      expect(cipher.cipher_text).to eq('hello world')
     end
 
     it 'rotates chars backwards' do
       expected = ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
         "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d"]
-      expect(cipher.rotate_chars(3, 'h')).to eq(expected)
+      expect(cipher.rotate_characters(3, 'h')).to eq(expected)
     end
 
     it 'creates arrays of characters with shifts' do
