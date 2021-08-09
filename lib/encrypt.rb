3f02ec89f626eb918e2a9dfe75f8f699c159e4a5
Henry Schmid <henry.schmid1@gmail.com>
2021-08-09 16:29:42 -0600

    Fix indentations

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,16 +4,16 @@ if ARGV != ['message.txt', 'encrypted.txt']
   puts "Please enter file names with the following format:\n
          message.txt encrypted.txt"
 else
-   message = File.read(ARGV[0]).chomp
-   key = '54728'
-   date = '231230'
+  message = File.read(ARGV[0]).chomp
+  key = '54728'
+  date = '231230'
 
-   enigma = Enigma.new
-   enigma.encrypt(message, key, date)
+  enigma = Enigma.new
+  enigma.encrypt(message, key, date)
 
-   encrypted = File.open('encrypted.txt', 'w')
-   encrypted.write(enigma.encrypt(message, key, date)[:encryption])
-   encrypted.close
+  encrypted = File.open('encrypted.txt', 'w')
+  encrypted.write(enigma.encrypt(message, key, date)[:encryption])
+  encrypted.close
 
-   puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
+  puts "Created 'encrypted.txt' with the key #{key} and date #{date}"
 end
